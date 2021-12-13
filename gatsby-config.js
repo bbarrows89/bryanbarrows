53cd374cb28fc58268cdbb1294a5e4d18f8c2393
bbarrows89 <bryanbarrows@live.com>
2021-12-13 15:55:13 -0800

    Initial commit from gatsby: (https://github.com/sebsojeda/gatsby-starter-purist.git)

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    name: "Purist",
+    defaultTitle: "Purist: Creating content",
+    titleTemplate: "%s • Purist: Creating content",
+    description: "A simple way to create content with Gatsby",
+    hero: {
+      heading: "Welcome to Purist.",
+      subHeading: "A simple way to create content with Gatsby",
+    },
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/sebsojeda",
+      },
+      {
+        name: "github",
+        url: "https://github.com/sebsojeda/gatsby-theme-purist",
+      },
+    ],
+  },
+  plugins: ["gatsby-theme-purist"],
+};
