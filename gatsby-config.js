de884bb681606682bf24adf07f39a579cd90f28d
bbarrows89 <bryanbarrows@live.com>
2021-12-14 00:29:38 -0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,130 @@
 module.exports = {
   siteMetadata: {
-    name: "Purist",
-    defaultTitle: "Purist: Creating content",
-    titleTemplate: "%s • Purist: Creating content",
-    description: "A simple way to create content with Gatsby",
+    siteUrl: 'https://bryanbarrows.me',
+    name: "Bryan Barrows",
+    defaultTitle: "Bryan Barrows",
+    titleTemplate: "%s • Bryan Barrows",
+    description: "The blog and website of Bryan Barrows, Workflows Advocate at Okta, based in Seattle WA",
     hero: {
-      heading: "Welcome to Purist.",
-      subHeading: "A simple way to create content with Gatsby",
+      heading: "Bryan Barrows",
+      subHeading: "Workflows Advocate at Okta - Based in Seattle, WA",
     },
     social: [
       {
         name: "twitter",
-        url: "https://twitter.com/sebsojeda",
+        url: "https://twitter.com/bryanbarrows",
       },
       {
-        name: "github",
-        url: "https://github.com/sebsojeda/gatsby-theme-purist",
+        name: "linkedin",
+        url: "https://linkedin.com/in/bbarrows89",
+      },
+      {
+        name: "devto",
+        url: "https://dev.to/bryanbarrows",
+      },
+      {
+        name: "youtube",
+        url: "https://www.youtube.com/channel/UCGXRZbU_2IWFF7KopaVO02g",
       },
     ],
   },
-  plugins: ["gatsby-theme-purist"],
-};
+  plugins: [
+    {
+      resolve: 'gatsby-theme-purist',
+      options: {
+        contentBase: 'content',
+        basePath: '/',
+        featuredArticleLimit: 2,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Bryan Barrows`,
+        short_name: `Bryan Barrows`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/gatsby-theme-purity/components/Logo/favicon.svg',
+        cache_busting_mode: 'none'
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/content/articles`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed-mdx`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.frontmatter.description,
+                  date: edge.node.frontmatter.date,
+                  author: site.siteMetadata.name,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }]
+                });
+              });
+            },
+            query: `
+              {
+                allMdx(
+                  sort: { order: DESC, fields: [frontmatter___date] }, filter: {frontmatter: { draft: {eq: false} }}
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                        description
+                        author
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Bryan Barrows RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/articles/"
+          }
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+         workboxConfig: {
+            globPatterns: ['**/icon-path*']
+        }
+      }
+    }
+  ],
+}
